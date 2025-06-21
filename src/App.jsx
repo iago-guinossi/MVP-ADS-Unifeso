ce8ccd913cafa93b18e279fb05c6a15b98b91638
iago-guinossi <108906641+iago-guinossi@users.noreply.github.com>
2025-06-20 22:32:29 -0300

    feat: create and add component navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { NavBar } from "./components/NavBar/NavBar"
+
 function App() {
 
   return (
     <>
-      
+      <NavBar/>
     </>
   )
 }
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+import logo from '../../assets/logo.jpeg'
+import { NavButton } from "../NavButton";
+
+const Container = styled.div`
+    height: 120px;
+    width: 100vw;
+    background-color: var(--white);
+    display: flex;
+    box-sizing: border-box;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 20px;
+`
+
+const Logo = styled.img`
+    width: 80px;
+    height: 80px;
+    border-radius: 100%;
+    overflow: hidden;
+`
+
+export function NavBar() {
+
+  return (
+    <Container>
+      <Logo src={logo} alt='Logo Teré Verde'/>
+      <NavButton/>
+    </Container>
+
+  )
+}
