9c4fce6d92727ab924b47fdb98134a010db0a8f9
iago-guinossi <108906641+iago-guinossi@users.noreply.github.com>
2025-06-22 15:59:14 -0300

    Update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
+import { Card } from "./components/Card/Card"
 import { Cover } from "./components/Cover"
 import { NavBar } from "./components/NavBar/NavBar"
+import trilhas from './trilhas/trilhas.json'
+import styled from "styled-components"
+import { useState } from "react"
+import { Modal } from "./components/Modal"
 
 function App() {
+
+  const [trilhaSelecionada, setTrilhaSelecionada] = useState(null);
+
+
+  const Container = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 20px;
+`
 
   return (
     <>
       <NavBar/>
       <Cover/>
+      <Container>
+        {
+          trilhas.map((trilha, index) => (
+            <Card key={index} {...trilha} onClick={() => setTrilhaSelecionada(trilha)}/>
+          ))
+        }
+      </Container>
+      {trilhaSelecionada && (
+    <Modal trilha={trilhaSelecionada} onClose={() => setTrilhaSelecionada(null)} />
+)}
+
     </>
   )
 }
